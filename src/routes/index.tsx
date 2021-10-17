77e2f97caf40cc0f674aec52922308954873c95e
reginaldoboeke <reginaldoboeke@gmail.com>
2021-10-17 15:57:49 -0300

    removed page used by tests

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,9 @@
 import { Switch, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
-import { Dashboard } from '../pages/Dashboard';
 
 export const Routes = () => (
   <Switch>
     <Route path="/" exact component={Home} />
-    <Route path="/dashboard" exact component={Dashboard} />
   </Switch>
 );
