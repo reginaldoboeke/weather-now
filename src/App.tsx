2b70e5bce4651f280ffd59d078353e3488694087
reginaldoboeke <reginaldoboeke@gmail.com>
2021-10-17 15:55:45 -0300

    refactor: moved header to app component; separate css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter } from 'react-router-dom';
 
+import { Header } from './components/Header';
 import { Routes } from './routes';
 
+import './styles/variables.scss';
 import './styles/global.scss';
+import './styles/app-styles.scoped.scss';
 
 export function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <Header />
+
+      <main>
+        <Routes />
+      </main>
     </BrowserRouter>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,13 @@
-import { Link } from 'react-router-dom';
-import { Header } from '../../components/Header';
 import { WeatherCard } from '../../components/WeatherCard';
 
 import './styles.scoped.scss';
 
 export function Home() {
   return (
-    <>
-      <Header />
-      <main>
-        <section>
-          <WeatherCard title="Nuuk, GL" locale="Nuuk" />
-          <WeatherCard title="Urubici, BR" locale="Urubici" />
-          <WeatherCard title="Nairobi, KE" locale="Nairobi" />
-        </section>
-        <Link to="/dashboard">
-          Dashboard
-        </Link>
-      </main>
-    </>
+    <section>
+      <WeatherCard title="Nuuk, GL" locale="Nuuk" />
+      <WeatherCard title="Urubici, BR" locale="Urubici" />
+      <WeatherCard title="Nairobi, KE" locale="Nairobi" />
+    </section>
   );
 }
