77e7699871057e4eb0fb3fe3cbcbe7274c9de519
reginaldoboeke <reginaldoboeke@gmail.com>
2022-02-13 13:07:46 -0300

    added more tests to home page

--- src/pages/Home/Home.spec.tsx
+++ src/pages/Home/Home.spec.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { mocked } from 'ts-jest/utils';
 
 import { useWeather } from '../../hooks/useWeather';
@@ -18,10 +18,51 @@ describe('Home page', () => {
     render(<Home />);
     
     const section = document.querySelector('section');
-    const cardsLength = section?.querySelectorAll('.card').length;
+    const cards = section?.querySelectorAll('.card');
+
+    fireEvent.mouseEnter(screen.getByText('Urubici, BR'));
 
     expect(section).not.toBeEmptyDOMElement();
-    expect(cardsLength).toEqual(3);
+    expect(cards?.length).toEqual(3);
+    expect(cards && cards[1].querySelector('.details')).not.toBe(null);
+  });
+
+  it('should renders details on mouse enter', () => {
+    const useWeatherMocked = mocked(useWeather);
+    useWeatherMocked.mockReturnValue({
+      isLoading: false,
+      isError: false,
+      weather: {} as any,
+    } as any);
+
+    render(<Home />);
+    
+    const section = document.querySelector('section');
+    const cards = section?.querySelectorAll('.card');
+
+    fireEvent.mouseEnter(screen.getByText('Nairobi, KE'));
+
+    expect(cards && cards[2].querySelector('.details')).not.toBe(null);
+  });
+
+  it('should hide details of all cards', () => {
+    const useWeatherMocked = mocked(useWeather);
+    useWeatherMocked.mockReturnValue({
+      isLoading: false,
+      isError: false,
+      weather: {} as any,
+    } as any);
+
+    render(<Home />);
+    
+    const section = document.querySelector('section');
+    const cards = section?.querySelectorAll('.card');
+
+    fireEvent.mouseLeave(screen.getByText('Urubici, BR'));
+
+    cards?.forEach(card => {
+      expect(card.querySelector('.details')).toBe(null);
+    });
   });
 });
 
